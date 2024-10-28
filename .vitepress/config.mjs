5f17193ba17e117e5787865c41bd74ae99dd3fef
OrenAlexandotus <1151825723@qq.com>
2024-10-28 22:57:53 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "AlexJTech's Library",
+  description: "一个使用 VitePress 驱动的个人笔记博客静态网站",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    footer: {
+      copyright:
+        'Copyright © 2024-present AlexJTech'
+    },
+
+    logo: '/logo.svg',
+  },
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  base: "/AlexJTech.github.io/",
+})
---
+++ index.md
@@ -0,0 +1,31 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "AlexJTech's Library"
+  text: "AlexJTech 的知识站点"
+  tagline: 一个使用 VitePress 驱动的个人知识站点
+  image:
+    src: /background.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: 开始阅读
+      link: /#
+    - theme: brand
+      text: markdown-examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
