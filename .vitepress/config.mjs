742db6a83ee895c10b93152d7c45cc1cea59fac1
OrenAlexandotus <1151825723@qq.com>
2024-10-29 00:54:11 +0800

    add 搜索功能模块

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "AlexJTech's Library",
-  description: "一个使用 VitePress 驱动的个人笔记博客静态网站",
+  description: '一个使用 VitePress 驱动的个人笔记博客静态网站',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,22 +16,42 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
     ],
 
     footer: {
-      copyright:
-        'Copyright © 2024-present AlexJTech'
+      copyright: 'Copyright © 2024-present AlexJTech',
     },
 
     logo: '/logo.svg',
+
+    // 设置搜索框的样式
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+            },
+          },
+        },
+      },
+    },
   },
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  head: [['link', { rel: 'icon', href: '/logo.svg' }]],
   // base: "/AlexJTech.github.io/",
 })
